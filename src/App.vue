bc3371aabf6e22d7a0b071614b237a707b027424
TarikoDan <62624974+TarikoDan@users.noreply.github.com>
2020-11-09 19:07:14 +0200

    v-once, v-model, v-if(v-else), :bind, @listen, v-for, :ref

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img width="100" alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="let's explore Vue.js !"/>
   </div>
 </template>
 
